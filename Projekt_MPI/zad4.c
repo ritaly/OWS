60b8e4f03c8a19d2242580694c2df11671f70117
ritaly <malgorzata.lyczywek@gmail.com>
2017-05-21 19:35:57 +0200

    zad 4

--- Projekt_MPI/zad4.c
+++ Projekt_MPI/zad4.c
@@ -1,133 +1,110 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <mpi.h>
 #include <stdlib.h>
-#include <omp.h> 
-#include <time.h> 
-
+#include <omp.h>
+#include <time.h>
 
 int main(int argc, char * argv[]) {
-  MPI_Status status;
-  char message[100];
-  int numprocs, rank, namelen;
-  double step;
-  int num_steps = 0;
-  int p_start, p_end;
-  double pi, tmp_pi, sum = 0.0;
- 
-  MPI_Init(0, 0);
-  MPI_Comm_size(MPI_COMM_WORLD, & numprocs);
-  MPI_Comm_rank(MPI_COMM_WORLD, & rank);
+        MPI_Status status;
+        char message[100];
+        int numprocs, rank, namelen;
+        double step;
+        int num_steps = 0;
+        int p_start, p_end;
+        double pi, tmp_pi, sum = 0.0,startwtime = 0.0, endwtime;
+        MPI_Init(0, 0);
+        MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+        MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-  if (rank == 0) {
+        if (rank == 0) {
+                /*fprintf(stdout, "Liczba krokow: (0 konczy prace): ");
+                fflush(stdout);
+                if (scanf("%d", &num_steps) != 1) {
+                        fprintf(stdout, "Koniec pracy\n");
+                        num_steps = 0;
+                }; */
+                num_steps = 1000000000;
+                fprintf(stdout, "Instancja: %d\n", num_steps);
+                //jeśli kroków >100  wysyła instację do pozostałych procesów, które uczestniczą w obliczeniach
+                if (num_steps > 100) {
+                        MPI_Bcast( &num_steps, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-    fprintf(stdout, "Liczba krokow: (0 konczy prace): ");
-    fflush(stdout);
-    if (scanf("%d", & num_steps) != 1) {
-      fprintf(stdout, "Koniec pracy\n");
-      num_steps = 0;
-    };
+                        p_start = (num_steps / numprocs) * rank;
+                        p_end = (num_steps / numprocs) * ((rank + 1));
+                        printf("\n%d : pocz = %d, koniec = %d\n", rank, p_start, p_end);
 
-    if (num_steps > 100) {
-      
-      MPI_Bcast( & num_steps, 1, MPI_INT, 0, MPI_COMM_WORLD);
+                        //pi counter
+                        double x;
+                        int i;
+                        step = 1. / (double) num_steps;
+                        startwtime = MPI_Wtime(); //czas w sekundach
+                        for (i = p_start; i < p_end; i++) {
+                                x = (i + .5) * step;
+                                sum = sum + 4.0 / (1. + x * x);
+                        }
 
-      p_start = (num_steps / numprocs) * rank;
-      p_end = (num_steps / numprocs) * ((rank + 1));
+                        tmp_pi = sum * step;
+                        //printf("Proces: %d - Wartosc liczby PI wynosi %15.12f\n", rank, tmp_pi);
+                        endwtime = MPI_Wtime();
+                        MPI_Reduce( &tmp_pi, &pi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 
-      printf("%d pocz = %d, koniec = %d\n", rank, p_start, p_end);
-	
-	//pi counter
+                        printf("\n*** Calkowita wartosc PI wynosi %f ***\n", pi);
+                        printf("* Calkowity czas przetwarzania wynosi %f sekund\n",((double)(endwtime  -  startwtime)));
+                }
 
-      clock_t start_t, end_t, total_t;
-      double x;
-      int i;
-      step = 1. / (double) num_steps;
-      start_t = clock();
-      
-	for (i = p_start; i < p_end; i++) 
-	{
-        x = (i + .5) * step;
-        sum = sum + 4.0 / (1. + x * x);
-	}
+                else //jesli mniej niz 100
+                {
 
-      tmp_pi = sum * step;
-      end_t = clock();
-      total_t = (double)(end_t - start_t) / CLOCKS_PER_SEC;
+                        p_start = 0;
+                        p_end = num_steps;
 
-      printf("\nProces: %d - Wartosc liczby PI wynosi %15.12f\n", rank, tmp_pi);
-      printf("Czas przetwarzania wynosi %f sekund\n", total_t);
+                        printf("\n%d : pocz = %d, koniec = %d\n", rank, p_start, p_end);
 
-      MPI_Reduce( &tmp_pi, &pi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+                        //pi counter
+                        double x;
+                        int i;
+                        startwtime = MPI_Wtime();   //czas w sekundach
+                        step = 1. / (double) num_steps;
 
-      printf("\n*** Calkowita wartosc PI wynosi %f ***\n", pi);
-      
+                        for (i = p_start; i < p_end; i++) {
+                                x = (i + .5) * step;
+                                sum = sum + 4.0 / (1. + x * x);
+                        }
 
-    }
+                        tmp_pi = sum * step;
+                        num_steps = 0;
+                        MPI_Bcast( &num_steps, 1, MPI_INT, 0, MPI_COMM_WORLD);
+                        endwtime = MPI_Wtime();
+                        printf("\n### Calkowita wartosc PI wynosi %f ###\n", pi);
+                        printf("# Calkowity czas przetwarzania wynosi %f sekund\n",((double)(endwtime  -  startwtime)));
 
-    else //jesli mniej niz 100
-    {
+                }
 
-      p_start = 0;
-      p_end = num_steps;
+        } else { //jesli nie jestem rootem
 
-      printf("%d : pocz = %d, koniec = %d\n", rank, p_start, p_end);
+                MPI_Bcast( &num_steps, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-	//pi counter
+                if (num_steps > 0) {
+                        p_start = (num_steps / numprocs) * rank;
+                        p_end = (num_steps / numprocs) * ((rank + 1));
 
-      clock_t start_t, end_t, total_t;
-      double x;
-      int i;
-      step = 1. / (double) num_steps;
-      start_t = clock();
-      for (i = p_start; i < p_end; i++) {
-        x = (i + .5) * step;
-        sum = sum + 4.0 / (1. + x * x);
-      }
+                        printf("\n%d : pocz = %d, koniec = %d\n", rank, p_start, p_end);
 
-      tmp_pi = sum * step;
-      end_t = clock();
-	total_t = (double)(end_t - start_t) / CLOCKS_PER_SEC;
+                        double x;
+                        int i;
+                        step = 1. / (double) num_steps;
 
-      printf("Czas przetwarzania wynosi %f sekund\n", total_t);
+                        for (i = p_start; i < p_end; i++) {
+                                x = (i + .5) * step;
+                                sum = sum + 4.0 / (1. + x * x);
+                        }
+                        tmp_pi = sum * step;
+                        //printf("\nProces: %d - Wartosc liczby PI wynosi %15.12f\n", rank, tmp_pi);
 
-      printf("\n^^^ Calkowita wartosc PI wynosi %f ^^^\n", tmp_pi);
+                        MPI_Reduce( &tmp_pi, &pi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+                }
 
-      num_steps = 0;
-      MPI_Bcast( &num_steps, 1, MPI_INT, 0, MPI_COMM_WORLD);
+        }
 
-    }
-
-  } else { //jesli nie jestem rootem 
-
-    MPI_Bcast( &num_steps, 1, MPI_INT, 0, MPI_COMM_WORLD);
-
-    if (num_steps > 0) {
-      p_start = (num_steps / numprocs) * rank;
-      p_end = (num_steps / numprocs) * ((rank + 1));
-
-      printf("%d : pocz = %d, koniec = %d\n", rank, p_start, p_end);
-
-      clock_t start_t, end_t, total_t;
-      double x;
-      int i;
-      step = 1. / (double) num_steps;
-      start_t = clock();
-      for (i = p_start; i < p_end; i++) {
-        x = (i + .5) * step;
-        sum = sum + 4.0 / (1. + x * x);
-      }
-
-      tmp_pi = sum * step;
-      end_t = clock();
-      total_t = (double)(end_t - start_t) / CLOCKS_PER_SEC;
-
-      printf("\nProces: %d - Wartosc liczby PI wynosi %15.12f\n", rank, tmp_pi);
-      printf("Czas przetwarzania wynosi %f sekund\n", total_t);
-
-      MPI_Reduce( &tmp_pi, &pi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-    }
-
-  }
-
-  MPI_Finalize();
+        MPI_Finalize();
 }
